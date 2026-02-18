ffe6a05917599f5dde47754dbc23f5e8b0835c6c
Cybli <peroutkb@oregonstate.edu>
2026-02-18 12:25:12 -0800

    Added ability to fetch a location with a specific id

--- backend/server.js
+++ backend/server.js
@@ -8,26 +8,36 @@ const app = express();
 const PORT = 8210
 
 //Database
-const db = require('./dbconnector');
+const pool = require('./dbconnector'); //
 
 //Communication to front end
 const cors = require('cors');
-const pool = require('./dbconnector');
 app.use(cors());
 
 /*
     ROUTES
 */
 
-//Locations
-app.get('/locations', async (req, res) => {
+//All Locations
+app.get('/locations', async (req, res) => { // the /locations address will fetch the locations from the database
     try{
         //Query the rows from the database
         const [rows] = await pool.query('SELECT * FROM location');
-        res.json(rows);
+        res.json(rows); //Return the result of the query in json format
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
+});
+
+//Locations with specific id
+app.get('/locations/:id', async (req, res) => { // /locations/ENTERID
+  try {
+    const [rows] = await pool.query('SELECT * FROM location WHERE id_location = ?', [req.params.id]);
+    if (rows.length === 0) return res.status(404).json({ error: 'Location not found' }); //Error if location is not found
+    res.json(rows[0]); //Return the result of the query in json format
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 app.listen(3000, () => {
