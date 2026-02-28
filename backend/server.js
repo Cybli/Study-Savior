23d4ddb687846555fa353b1c13764c62ecbad6ac
Samuel Vernick <samuel.vernicks@gmail.com>
2026-02-27 19:02:57 -0800

    Fixed linking issue with tags, impelemented functioning sidebar and modified styling

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 /*
-    SETUP
+  SETUP
 */
 
 //Express
@@ -20,18 +20,18 @@ const bcrypt = require('bcrypt');
 const SALT_ROUNDS = 10;
 
 /*
-    ROUTES
+  ROUTES
 */
 
 //All Locations
 app.get('/locations', async (req, res) => { // the /locations address will fetch the locations from the database
-    try{
-        //Query the rows from the database and convert single POINT to lng and lat
-        const [rows] = await pool.query('SELECT *, ST_X(coordinates_location) as lat, ST_Y(coordinates_location) as lng FROM location');
-        res.json(rows); //Return the result of the query in json format
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+  try{
+    //Query the rows from the database and convert single POINT to lng and lat
+    const [rows] = await pool.query('SELECT *, ST_X(coordinates_location) as lat, ST_Y(coordinates_location) as lng FROM location');
+    res.json(rows); //Return the result of the query in json format
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 //Locations with specific id
@@ -45,72 +45,84 @@ app.get('/locations/:id', async (req, res) => { // /locations/ENTERID
   }
 });
 
+//Get tags for a specific location
+app.get('/locations/:id/tags', async (req, res) => { // /locations/ENTERID/tags
+  try {
+    const [tags] = await pool.query(
+      'SELECT t.id_tag, t.name_tag FROM tag t INNER JOIN location_tags lt ON t.id_tag = lt.tag_id WHERE lt.location_id = ?',
+      [req.params.id]
+    );
+    res.json(tags); // Return the result of the query in json format
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 //Register new user
 /*
 Can return 3 things:
-    - 'Username already taken'
-    - 'Account created successfully'
-    - or an error message
+  - 'Username already taken'
+  - 'Account created successfully'
+  - or an error message
 */
 app.post('/register', async (req, res) => {
-    //Init vars to hold username and password
-    const { username, password } = req.body;
-    try {
-        //Check is username has been taken
-        const [existing] = await pool.query('SELECT * FROM user WHERE username_user = ?', [username]);
-        //If any matching usernames are found send error
-        if (existing.length > 0) return res.status(400).json({ error: 'Username already taken' });
+  //Init vars to hold username and password
+  const { username, password } = req.body;
+  try {
+    //Check is username has been taken
+    const [existing] = await pool.query('SELECT * FROM user WHERE username_user = ?', [username]);
+    //If any matching usernames are found send error
+    if (existing.length > 0) return res.status(400).json({ error: 'Username already taken' });
 
-        //Generate salt
-        const salt = await bcrypt.genSalt(SALT_ROUNDS);
-        //Hash password
-        const hashedPassword = await bcrypt.hash(password, salt);
+    //Generate salt
+    const salt = await bcrypt.genSalt(SALT_ROUNDS);
+    //Hash password
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-        //Insert into db
-        await pool.query(
-           'INSERT INTO user (username_user, salt_user, hashedpass_user) VALUES (?, ?, ?)',
-            [username, salt, hashedPassword] 
-        );
+    //Insert into db
+    await pool.query(
+      'INSERT INTO user (username_user, salt_user, hashedpass_user) VALUES (?, ?, ?)',
+      [username, salt, hashedPassword] 
+    );
 
-        res.json({ message: 'Account created successfully' });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+    res.json({ message: 'Account created successfully' });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 
 //Login a user
 /*
 Can return 3 things:
-    - 'Invalid username or password'
-    - 'Logic successful'
-    - or an error message
+  - 'Invalid username or password'
+  - 'Logic successful'
+  - or an error message
 */
 app.post('/login', async (req, res) => {
-    //Init vars to hold username and password
-    const { username, password } = req.body;
-    try{
-        //Find the user
-        const [rows] = await pool.query('SELECT * FROM user WHERE username_user = ?', [username]);
-        //If no user is found error
-        if (rows.length === 0) return res.status(401).json({ error: 'Invalid username or password' });
-        
-        const user = rows[0];
+  //Init vars to hold username and password
+  const { username, password } = req.body;
+  try{
+    //Find the user
+    const [rows] = await pool.query('SELECT * FROM user WHERE username_user = ?', [username]);
+    //If no user is found error
+    if (rows.length === 0) return res.status(401).json({ error: 'Invalid username or password' });
+    
+    const user = rows[0];
 
-        //Check for valid password
-        const match = await bcrypt.compare(password, user.hashedpass_user);
-        if (!match) return res.status(401).json({ error: 'Invalid username or password' });
+    //Check for valid password
+    const match = await bcrypt.compare(password, user.hashedpass_user);
+    if (!match) return res.status(401).json({ error: 'Invalid username or password' });
 
-        //If user is found return their id and username
-        res.json({ message: 'Login successful', id_user: user.id_user, username: user.username_user });
-    } catch (err) {
-        res.status(500).json({ error: err.message })
-    }
+    //If user is found return their id and username
+    res.json({ message: 'Login successful', id_user: user.id_user, username: user.username_user });
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
 });
 
 
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,29 +1,31 @@
 import { useEffect, useRef, useState } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import { LocationSidebar } from './popups/LocationSidebar';
 
 function App() {
-    const mapContainer = useRef(null);
-    const map = useRef(null);
-    const [locations, setLocations] = useState([]);
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [locations, setLocations] = useState([]);
+  const [selectedLocation, setSelectedLocation] = useState(null);
 
-    /*To use:
-        To use information stored in a location use location.NAME_OF_COLUMN_IN_DATABASE
-        For example:
-            To get the name of a location use location.name_location
-    */
+  /*To use:
+      To use information stored in a location use location.NAME_OF_COLUMN_IN_DATABASE
+      For example:
+          To get the name of a location use location.name_location
+  */
 
-    //Fetch locations from backend
-    useEffect(() => {
-        //TODO: REPLACE WITH BACKEND URL
-        //TODO: USE HTTPS FOR ENCRYPTION SENDING USER PASSWORDS
-        fetch('http://localhost:4000/locations')
-            .then(res => res.json())            //Gather information in json format
-            .then(data => {
-                setLocations(data);  //Store data in setLocations
-            })
-            .catch(err => console.error('Failed to fetch locations:', err));
-    }, []);
+  //Fetch locations from backend
+  useEffect(() => {
+    //TODO: REPLACE WITH BACKEND URL
+    //TODO: USE HTTPS FOR ENCRYPTION SENDING USER PASSWORDS
+    fetch('http://localhost:4000/locations')
+      .then(res => res.json())            //Gather information in json format
+      .then(data => {
+        setLocations(data);  //Store data in setLocations
+      })
+      .catch(err => console.error('Failed to fetch locations:', err));
+  }, []);
 
   useEffect(() => {
     // Return map a single time
@@ -35,52 +37,55 @@ function App() {
 
     // Init map
     map.current = L.map(mapContainer.current).setView(osuCoords, zoomLevel);
-        // Add OSM tile layer (z = zoom, x = horizontal tile coordinate, y = vertical tile coordinate)
-        // Values automatically updated by leaflet as the user pans and zooms
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            // Required by OSM's license, adds copyright to bottom right of page
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-            // Sets the number of tiles/columns kept when panning
-            keepBuffer: 100
-        }).addTo(map.current);
-    }, []);
+    // Add OSM tile layer (z = zoom, x = horizontal tile coordinate, y = vertical tile coordinate)
+    // Values automatically updated by leaflet as the user pans and zooms
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      // Required by OSM's license, adds copyright to bottom right of page
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      // Sets the number of tiles/columns kept when panning
+      keepBuffer: 100
+    }).addTo(map.current);
+  }, []);
 
-    // Add location markers to map
-    useEffect(() => {
-        if (!map.current || locations.length === 0) return; //Check if map exists and there are locations
-        //Wait for map to load
-        map.current.whenReady(() => {
-            //For each location in the database
-            locations.forEach(location => {
-                if (!location.lat || !location.lng) return; //Check if there are latitude and longitude in the db
-                const marker = L.marker([location.lat, location.lng]).addTo(map.current); //Create a marker and add it to the map
-                //Populate the marker with info from database
-                //TODO: REPLACE WITH SIDE BAR
-                marker.bindPopup(`
-                        <b>${location.name_location}</b><br>
-                        ${location.description_location}<br>
-                        Rating: ${location.average_rating_location ?? 'No ratings yet'}
-                `);
-            });
-    }, [locations]);
+  // Add location markers to map
+  useEffect(() => {
+    if (!map.current || locations.length === 0) return; //Check if map exists and there are locations
+    //Wait for map to load
+    map.current.whenReady(() => {
+      //For each location in the database
+      locations.forEach(location => {
+        if (!location.lat || !location.lng) return; //Check if there are latitude and longitude in the db
+        const marker = L.marker([location.lat, location.lng]).addTo(map.current); //Create a marker and add it to the map
+        
+        // Add click event to show location in sidebar
+        marker.on('click', () => {
+          setSelectedLocation(location);
+        });
+      });
+    });
+  }, [locations]);
 
-    
-});
-
-    return(
-        <div className="w-full h-screen flex flex-col">
-            <div className="bg-gradient-to-r from-orange-600 to-orange-500 p-4 shadow-lg">
-                <h1 className="text-white text-3xl font-bold">Study Savior</h1>
-                <p className="text-orange-100">OSU Campus Study Locations</p>
-            </div>
-            <div 
-                ref={mapContainer} 
-                className="w-full flex-1"
-                style={{ height: '100%' }}
-            />
-        </div>
-    );
+  return(
+    <div className="w-full h-screen flex flex-col">
+      <div className="bg-linear-to-r from-orange-600 to-orange-500 p-4 shadow-lg">
+        <h1 className="text-white text-3xl font-bold">Study Savior</h1>
+        <p className="text-orange-100">OSU Campus Study Locations</p>
+      </div>
+      <div className="relative flex-1">
+        <div 
+          ref={mapContainer} 
+          className="w-full h-full"
+        />
+        {selectedLocation && (
+          <LocationSidebar 
+            location={selectedLocation} 
+            onClose={() => setSelectedLocation(null)} 
+          />
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App
