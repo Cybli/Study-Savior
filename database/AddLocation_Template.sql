1e725f99f5f1aab62a8f820215a2a1472b62d0b0
Sophie Peroutka <peroutkb@oregonstate.edu>
2026-02-28 17:53:22 -0800

    Add SQL template for location and tag insertion

---
+++ database/AddLocation_Template.sql
@@ -0,0 +1,34 @@
+-- Tip: you must remove the '<' and '>' when editing the template
+
+-- =========================
+-- STEP 1: Add the location
+-- =========================
+INSERT INTO location (name_location, description_location, hall_location, coordinates_location, image_path_location)
+VALUES (
+  '<LOCATION NAME>',           
+  '<DESCRIPTION>',             
+  '<BUILDING NAME>',           
+  ST_GeomFromText('POINT(<LAT> <LNG>)')
+  -- Tip: Right click a location in Google Maps and click the coordinates to copy them
+  '/images/<FILE NAME>'
+);
+
+-- =========================
+-- STEP 2: Add tags
+-- =========================
+-- Add or remove lines below depending on how many tags this location has
+-- This accepts new and already used tags
+INSERT IGNORE INTO tag (name_tag) VALUES ('<TAG 1>');
+INSERT IGNORE INTO tag (name_tag) VALUES ('<TAG 2>');
+INSERT IGNORE INTO tag (name_tag) VALUES ('<TAG 3>');
+
+-- =========================
+-- STEP 3: Link tags to location
+-- =========================
+-- Make sure the tag names here match exactly what you put in Step 2
+INSERT INTO location_tags (location_id, tag_id)
+SELECT LAST_INSERT_ID(), id_tag FROM tag WHERE name_tag IN (
+  '<TAG 1>',
+  '<TAG 2>',
+  '<TAG 3>'
+);
